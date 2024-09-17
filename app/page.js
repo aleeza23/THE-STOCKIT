a244cebe128f9eec8595a20fb971f0859733ed20
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-09-17 01:27:03 -0800

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,38 @@
+import dynamic from 'next/dynamic';
+
+// Critical components for initial load
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+
+// Dynamically imported components
+const Projects = dynamic(() => import('@/components/Projects'), {
+  ssr: false,
+});
+const Services = dynamic(() => import('@/components/Services'));
+const Tech = dynamic(() => import('@/components/Tech'));
+const DigitalMarketing = dynamic(() => import('@/components/DigitalMarketing'));
+const Teams = dynamic(() => import('@/components/Teams'));
+const Testimonials = dynamic(() => import('@/components/Testimonials'));
+const Blogs = dynamic(() => import('@/components/Blogs'));
+const ContactUs = dynamic(() => import('@/components/ContactUs'));
+const Footer = dynamic(() => import('@/components/Footer'));
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-gray-100 pt-10 pb-16">
+        <Header />
+        <Hero />
+      </div>
+      <Services />
+      <Tech />
+      <Projects />
+      <DigitalMarketing />
+      <Teams />
+      <Testimonials />
+      <Blogs />
+      <ContactUs />
+      <Footer />
+    </>
+  );
+}
